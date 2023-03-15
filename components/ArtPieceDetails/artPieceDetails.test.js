07b7baecbfa181865028255776ba38b2f5ec0a89
Efendi Varol <efendiv@icloud.com>
2023-03-15 12:58:59 +0100

    add testing

--- components/ArtPieceDetails/artPieceDetails.test.js
+++ components/ArtPieceDetails/artPieceDetails.test.js
@@ -1,2 +1,20 @@
 import ArtPieceDetails from "./index";
 import { render, screen } from "@testing-library/react";
+
+test("render Artpieces and find props [image, title, artist]", () => {
+  render(
+    <ArtPieceDetails
+      title="Orange Red and Green Abstract Painting"
+      artist="Steve Johnson"
+      image="https://example-apis.vercel.app/assets/art/orange-red-and-green.jpg"
+    />
+  );
+
+  const image = screen.getByRole("img");
+  const description = screen.getByText(
+    "Orange Red and Green Abstract Painting Steve Johnson"
+  );
+
+  expect(image).toBeInTheDocument();
+  expect(description).toBeInTheDocument();
+});
--- components/ArtPieceDetails/index.js
+++ components/ArtPieceDetails/index.js
@@ -4,11 +4,16 @@ import Image from "next/image";
 export default function ArtPieceDetails({ image, title, artist }) {
   return (
     <div>
-      <Image src={image} alt="Picture of the author" width={500} height={500} />
-      <div>
-        <span>{title}</span>
-        <span>{artist}</span>
-      </div>
+      <Image
+        priority={true}
+        src={image}
+        alt="Picture of the author"
+        width={500}
+        height={500}
+      />
+      <p>
+        {title} {artist}
+      </p>
     </div>
   );
 }
