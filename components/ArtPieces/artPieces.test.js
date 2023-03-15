0d345049b787ffae0dcfe6fd1222101d843543ac
Efendi Varol <efendiv@icloud.com>
2023-03-15 16:14:23 +0100

    add Spotlight + tests

--- components/ArtPieces/artPieces.test.js
+++ components/ArtPieces/artPieces.test.js
@@ -1,2 +1,25 @@
 import ArtPieces from "./index";
 import { render, screen } from "@testing-library/react";
+
+test("render Artpieces and find props [image, title, artist]", () => {
+  render(
+    <ArtPieces
+      pieces={[
+        {
+          name: "Orange Red and Green Abstract Painting",
+          imageSource: "/teveJohnson",
+          artist: "Steve Johnson",
+        },
+        {
+          name: "Orange Red and Green Abstract",
+          imageSource: "/SteveJohnson",
+          artist: "Steve",
+        },
+      ]}
+    />
+  );
+
+  const listItems = screen.getAllByRole("listitem");
+
+  expect(listItems).toHaveLength(2);
+});
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,26 @@
 import ArtPieces from "../components/ArtPieces";
 import useSWR from "swr";
+import SpotlightPiece from "../components/Spotlight";
 
 const URL = "https://example-apis.vercel.app/api/art";
 
 export default function HomePage() {
   const { data, isLoading, error } = useSWR(URL);
-  console.log(data);
 
-  // if (error) return <div>failed to load</div>;
+  function generateRandomIndex(datalength) {
+    return Math.floor(Math.random() * datalength);
+  }
+  console.log(data);
   if (isLoading) return <div>loading...</div>;
+  if (error) return <div>Page Load fails</div>;
 
   return (
     <div>
+      <h1>Art Gallery</h1>
+      <SpotlightPiece
+        image={data[generateRandomIndex(data.length)].imageSource}
+        artist={data[generateRandomIndex(data.length)].artist}
+      />
       <ArtPieces pieces={data} />
     </div>
   );
